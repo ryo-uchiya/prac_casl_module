88206dcd805bfbc5556f920f6ba86d626bb54d6c
Ryo Uchiya <73949988+ryo-uchiya@users.noreply.github.com>
2022-09-27 22:31:13 +0900

    Create read_file.c

---
+++ read_file.c
@@ -0,0 +1,22 @@
+void read_file(){
+    FILE *fp;
+
+    char *filename;
+
+    filename = "sample.cas";
+
+    //改行文字の削除
+    strtok(filename,"\n");
+
+    //ファイルを開く
+    fp = fopen(filename,"r");
+
+    if(fp == NULL){
+        printf("%s not found",filename);
+        return;
+    }
+    
+    rows_count(fp,filename,"r");
+
+    fclose(fp);
+}
