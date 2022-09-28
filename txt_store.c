6cee2f5214b724e39d3bd69c15a37a02764a25bf
Ryo Uchiya <73949988+ryo-uchiya@users.noreply.github.com>
2022-09-28 10:06:43 +0900

    Update txt_store.c

--- txt_store.c
+++ txt_store.c
@@ -1,11 +1,11 @@
 //txtファイル保存関数
 void txt_store(char *txt_list){
-    //txtファイルに書き込み
     FILE *txtfile;
     txtfile = fopen("sample.txt","w");
     if(txtfile==NULL){
     	printf("Can't write!");
     }
+    //txtファイルに書き込み
     fprintf(txtfile, "%s", txt_list);
     fclose(txtfile);
 
